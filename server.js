dc652409de5f3571919f10e7a8b54ecdad51679b
chrispetito <christopherpetito@gmail.com>
2019-05-29 11:22:44 -0700

    add server listen, logger middleware

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`This is a ${req.method} request at ${req.originalUrl} URL, performed at ${Date.now()}`)
+  next();
 };
 
 module.exports = server;
