9e3af14e13ef5aff8b5ebb52902f80e3beeb397c
chrispetito <christopherpetito@gmail.com>
2019-05-30 11:15:22 -0700

    add changes for heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
 const server = require('./server');
 
-server.listen(4000, () => {
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
     console.log('Server running on 4k');
 })
